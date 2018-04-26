2dd231ec9c9a4a1e89d3ae3ca551c13fce3d2fed
Tisha Murvihill <tmurv@take2tech.ca>
2018-04-25 20:13:25 -0600

    end-of-day commit

--- js/script.js
+++ js/script.js
@@ -3,23 +3,7 @@
 "use strict";
 
 $(document).ready(function () {
-
-    //Show_Hide Glossary Button Click 
-    $(".js--sectionGlossary__viewBtnCont--viewBtn").click(function () {
-        $(".glossary").toggleClass("u-hideItem");
-        $(".glossary__sticky").css("display", "none");      
-    });
-
-    //Show sticky header row in glossary
-    $(".js--showSticky").waypoint(function(direction) {
-        if (direction == "down") {
-            $(".glossary__sticky").css("display", "flex");
-        } else {
-            $(".glossary__sticky").css("display", "none");
-        }
-    });
-
-    //Show_Hide mainNav menu                                     
+    //Header page--Show_Hide mainNav menu                                     
     $(".js--nav__mobileNav--icon").click(function () {
 
         var mainNav = $(".js--main-nav");
@@ -28,7 +12,6 @@ $(document).ready(function () {
         var close = $(".nav__mobileNav--icon-close");
 
         mainNav.slideToggle(200);
-        mainNav.addClass("mainBlock")
 
         if ((hamburgerVis).length == 0) {
             hamburger.css("display", "block");
@@ -36,12 +19,39 @@ $(document).ready(function () {
 
         }
         else {
-
             hamburger.css("display", "none");
             close.css("display", "block");
         }
 
     });
+
+    //Projects page--Show technical deets popups for touch screens
+
+    $(".js--cardFrontTechnical").click(function (popupNumber) {
+        $(".js--popup1").css("display", "none");
+    });
+
+    $(".js--popBack").click(function (popupNumber) {
+       $(".js--popup1").css("display", "flex");
+    });
+
+
+    //Show_Hide Glossary Button Click 
+    $(".js--sectionGlossary__viewBtnCont--viewBtn").click(function () {
+        $(".glossary").toggleClass("u-hideItem");
+        $(".glossary__sticky").css("display", "none");
+    });
+
+    //Show sticky header row in glossary
+    $(".js--showSticky").waypoint(function (direction) {
+        if (direction == "down") {
+            $(".glossary__sticky").css("display", "flex");
+        } else {
+            $(".glossary__sticky").css("display", "none");
+        }
+    });
+
+
 
 });
 
